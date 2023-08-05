ae0426b4fc197f95fe0434f5fc746c655fa684f6
BranBayou <berhan.baye@gmail.com>
2023-08-05 21:15:40 +0300

    Add pointer

--- app.js
+++ app.js
@@ -18,3 +18,29 @@ console.log(cookieOne);
 console.log(cookieOne.color);
 console.log(cookieTow.color);
 
+// Pointers
+
+let num1 = 5;
+let num2 = num1;
+
+console.log(num1);
+console.log(num2);
+
+num1 = 10;
+console.log(num1);
+console.log(num2);
+
+let obj1 = {
+  value: 'foo'
+}
+let obj2 = obj1;
+
+console.log(obj1);
+console.log(obj2);
+
+obj1.value = 'bar'
+
+console.log(obj1);
+console.log(obj2);
+
+//
