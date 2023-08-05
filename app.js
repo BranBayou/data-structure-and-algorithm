1573439c373a760b2867aa04552eeb8bd6045de7
BranBayou <berhan.baye@gmail.com>
2023-08-06 01:32:43 +0300

    Add linked list constructor

--- app.js
+++ app.js
@@ -43,4 +43,22 @@ obj1.value = 'bar'
 console.log(obj1);
 console.log(obj2);
 
-//
+//Linked list
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+let myLinkedList = new LinkedList(4);
+console.log(myLinkedList);
