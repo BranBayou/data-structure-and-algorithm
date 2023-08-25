502ec62be13ef81dc029a608fbd1b9e8aad6ff82
BranBayou <berhan.baye@gmail.com>
2023-08-25 15:02:12 +0300

    Add doubly linked list constructor

---
+++ doublyLinkedList.js
@@ -0,0 +1,18 @@
+// Node
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    const newNode = new Node(value);
+    this.head = newNode;
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
