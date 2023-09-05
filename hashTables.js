6a4c8adce3ac85eb2ceb5b4ca3bc741daac35aee
BranBayou <berhan.baye@gmail.com>
2023-09-05 06:27:44 +0300

    Add get and keys method

--- hashTables.js
+++ hashTables.js
@@ -19,10 +19,37 @@ class HashTable {
     this.dataMap[index].push(key, value);
     return this;
   }
+
+  get(key) {
+    let index = this._hash(key)
+    if(this.dataMap[index]) {
+        for(let i = 0; i < this.dataMap[index].length; i++) {
+            if(this.dataMap[index][i][0] === key) {
+                return this.dataMap[index][i][1]
+            }
+         }
+     }
+     return undefined
+ }
+
+ keys() {
+  let allKeys = [];
+  for(let i = 0; i < this.dataMap.length; i++) {
+    if(this.dataMap[i]) {
+      for(let j = 0; j < this.dataMap[i].length; j++) {
+        allKeys.push(this.dataMap[i][j][0]);
+      }
+    }
+  }
+  return allKeys; 
+ }
 }
 
 let myHashTable = new HashTable();
 myHashTable.set('lumber', 251);
 myHashTable.set('washers', 40);
 myHashTable.set('bolts', 1100);
+
 console.log(myHashTable);
+console.log(myHashTable.get('washers'));
+console.log(myHashTable.keys());
