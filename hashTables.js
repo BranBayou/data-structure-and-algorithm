b38e3320c804e89bef1fc8c3b2d660345a70e452
BranBayou <berhan.baye@gmail.com>
2023-09-05 21:04:22 +0300

    Add interview question

--- hashTables.js
+++ hashTables.js
@@ -53,3 +53,34 @@ myHashTable.set('bolts', 1100);
 console.log(myHashTable);
 console.log(myHashTable.get('washers'));
 console.log(myHashTable.keys());
+
+// Interview questions
+
+let arr1 = [1, 3, 5];
+let arr2 = [2 ,4 ,5];
+
+function itemInCommon(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++){
+      if(arr1[i] === arr2[j]) return true;
+    }
+  }
+  return false;
+}
+
+console.log(itemInCommon(arr1, arr2));
+
+// Method 2
+
+function itemInCommon2(arr1, arr2) {
+  let obj = {};
+  for(let i = 0; i < arr1.length; i++){
+    obj[arr1[i]] = true;
+  }
+  for(let j = 0; j < arr2.length; j++){
+    if(obj[arr2[j]]) return true;
+  }
+  return false;
+}
+
+console.log(itemInCommon2(arr1, arr2));
