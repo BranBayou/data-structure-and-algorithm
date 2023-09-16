7f1435ac9fa5f495a7ae4f86b44f8ef8eb8d976e
BranBayou <berhan.baye@gmail.com>
2023-09-16 08:19:58 +0300

    Add selection sort

--- algorithms/bubbleSort.js
+++ algorithms/bubbleSort.js
@@ -1,9 +1,10 @@
-// Bubble sort
+// Basic sorts
 
+// Bubble sort
 function bubbleSort(array) {
-  for(let i = array.length - 1; i > 0; i--) {
-    for(let j = 0; j < array.length; j++) {
-      if(array[j] > array[j + 1]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[j + 1]) {
         let temp = array[j];
         array[j] = array[j + 1];
         array[j + 1] = temp;
@@ -13,4 +14,24 @@ function bubbleSort(array) {
   return array;
 }
 
-console.log(bubbleSort([3,6,1,4,2,5]));
+console.log(bubbleSort([3, 6, 1, 4, 2, 5]));
+
+// Selection sort
+
+function selectionSort(array) {
+  let min;
+  for (let i = 0; i < array.length - 1; i++) {
+    min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) min = j;
+    }
+    if (i !== min) {
+      let temp = array[i];
+      array[i] = array[min];
+      array[min] = temp;
+    }
+  }
+  return array;
+}
+
+console.log(selectionSort([3,5,1,2,4]));
